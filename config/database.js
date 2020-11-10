5014f77f3ce043e279e8e6c8510a9d9a84eee618
bboynam99 <30984515+bboynam99@users.noreply.github.com>
2020-11-10 17:09:16 +0700

    update

---
+++ config/database.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+	'url': 'mongodb+srv://bigsmall-shard-00-02.zoqdj.mongodb.net/test',
+	'options': {
+		'user':   'bigsmall',
+		'pass':   '2020*#',
+		'dbName': 'admin',
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
