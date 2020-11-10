5154c97e263261352becff41df7ee060e6a7910b
bboynam99 <30984515+bboynam99@users.noreply.github.com>
2020-11-10 17:43:06 +0700

    update

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-	'url': 'mongodb+srv://bigsmall:2020abcd@bigsmall.zoqdj.mongodb.net/test?retryWrites=true&w=majority',
+	'url': 'mongodb+srv://bigsmall-shard-00-00.zoqdj.mongodb.net:27017/test',
 	'options': {
 		'user':   'bigsmall',
 		'pass':   '2020abcd',
