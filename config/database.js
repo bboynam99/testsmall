517fad9c5653e154971a3863f9f2833a34c8f532
bboynam99 <30984515+bboynam99@users.noreply.github.com>
2020-11-18 17:38:27 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-	'url': 'mongodb+srv://bigsmall:2020abcd@bigsmall.zoqdj.mongodb.net/test'
+	'url': 'mongodb+srv://bigsmall.zoqdj.mongodb.net/test'
 	'options': {
 		'user':   'bigsmall',
 		'pass':   '2020abcd',
